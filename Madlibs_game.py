c57ba017d75fd5e41092c9f92f28b1903803f2cf
Amit Tambulkar <amittambulkar104@gmail.com>
2023-08-22 20:30:24 +0530

    Madlibs game
    A simple madlibs game.

---
+++ Madlibs_game.py
@@ -0,0 +1,11 @@
+# Simple Madlibs game.
+
+noun1 = input("Enter a noun: ")
+verb1 = input("Enter a verb: ")
+actor_name = input("Enter a actors name: ")
+movie_name = input("Enter a name of movie: ")
+
+print("He had " + noun1 + " in his mind.")
+print("He was " + verb1 + ".")
+print(actor_name + " was his friend.")
+print("They were in " + movie_name + " together.")
